acedf5f2514815af00a38b1df82353c4c0d70b8b
GemelliAngelo <gemeangelo27@gmail.com>
2025-04-04 16:14:51 +0200

    complete destroy

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,8 +4,15 @@
 
 @section('content')
     
-    <div class="d-flex justify-content-end">
+    <div class="d-flex justify-content-end gap-3">
         <a href="{{route("projects.edit",$project)}}" class="btn btn-warning">Modifica</a>
+        <form action="{{route("projects.destroy",$project)}}" method="POST">
+            @csrf
+            @method("DELETE")
+
+            <input type="submit" value="Elimina Progetto" class="btn btn-danger">
+
+        </form>
     </div>
 
     <h2 class="my-3">
