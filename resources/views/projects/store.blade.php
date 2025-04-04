df4d27e9f64330a87422efd71d51e64fe253531a
GemelliAngelo <gemeangelo27@gmail.com>
2025-04-04 15:33:58 +0200

    fix form style

--- resources/views/projects/store.blade.php
+++ resources/views/projects/store.blade.php
@@ -4,26 +4,26 @@
 
 @section('content')
     
-    <form class="p-3" action="{{route("projects.store")}}" method="post">
+    <form class="bg-secondary p-5" action="{{route("projects.store")}}" method="post">
 
-        <div class="form-control bg-body-tertiary mb-3">
+        <div class="form-control bg-body-tertiary mb-4">
             <label class="form-label" for="name">Nome</label>
-            <input class="form-control" type="text" name="name" id="name">
+            <input class="form-control" type="text" name="name" id="name" required>
         </div>
-        <div class="form-control bg-body-tertiary mb-3">
+        <div class="form-control bg-body-tertiary mb-4">
             <label class="form-label" for="client">Cliente</label>
             <input class="form-control" type="text" name="client" id="client">
         </div>
-        <div class="form-control bg-body-tertiary mb-3">
+        <div class="form-control bg-body-tertiary mb-4">
             <label class="form-label" for="worked_on_date">Periodo</label>
-            <input class="form-control" type="date" name="worked_on_date" id="worked_on_date">
+            <input class="form-control" type="date" name="worked_on_date" id="worked_on_date" required>
         </div>
-        <div class="form-control bg-body-tertiary mb-3">
+        <div class="form-control bg-body-tertiary mb-4">
             <label class="form-label" for="content">Riassunto</label>
-            <textarea class="form-control" name="content" id="content"></textarea>
+            <textarea class="form-control" name="content" id="content" required></textarea>
         </div>
 
-        <div class="mb-3 text-end w-100">
+        <div class="text-end w-100">
             <input type="submit" value="Aggiungi" class="btn btn-success w-100">
         </div>
 
