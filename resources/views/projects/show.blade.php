4f51519ff3ea1df7d34ee00f902e5a06143622a2
GemelliAngelo <gemeangelo27@gmail.com>
2025-04-04 16:20:37 +0200

    add delete modal + style

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,13 +6,10 @@
     
     <div class="d-flex justify-content-end gap-3">
         <a href="{{route("projects.edit",$project)}}" class="btn btn-warning">Modifica</a>
-        <form action="{{route("projects.destroy",$project)}}" method="POST">
-            @csrf
-            @method("DELETE")
-
-            <input type="submit" value="Elimina Progetto" class="btn btn-danger">
-
-        </form>
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
+            Elimina Progetto
+        </button>
+        
     </div>
 
     <h2 class="my-3">
@@ -25,5 +22,32 @@
     <p>{{$project->content}}</p>
 
     <a href="{{route("projects.index")}}">Tutti i progetti</a>
+
+
+
+
+
+    <div id="deleteModal" class="modal" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Conferma Eliminazione</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <p>Sei sicuro di voler eliminare il progetto?</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                <form action="{{route("projects.destroy",$project)}}" method="POST">
+                    @csrf
+                    @method("DELETE")
+    
+                    <input type="submit" value="Elimina" class="btn btn-danger">
+                </form>
+            </div>
+          </div>
+        </div>
+      </div>
 
 @endsection
