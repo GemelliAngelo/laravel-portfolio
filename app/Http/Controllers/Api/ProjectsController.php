77b0f2b18a340bb6d99ea12b3c712c9514618b35
GemelliAngelo <gemeangelo27@gmail.com>
2025-04-10 11:15:19 +0200

    addd api projects controller + route

---
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class ProjectsController extends Controller
+{
+    public function index(){
+        return "sei nella api index";
+    }
+}
